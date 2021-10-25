cc247168085bbca1a3a7f449ff4ad50f74f02a2e
Craig Ronald <craig@aimermedia.com>
2021-10-25 11:07:32 +0100

    Your Message

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,65 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Component } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+import { Virtuoso } from 'react-virtuoso'
+
+type Props = {}
+type State = {}
+
+class Tab1 extends Component <Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+
+
+  }
+
+  generateTest() {
+
+    let array = [];
+
+    for (let i = 0; i < 200; i++) {
+      array.push(i);
+    }
+
+    return array
+
+  }
+
+  render() {
+
+    return (
+      <IonContent>
+
+        <Virtuoso
+          useWindowScroll
+          data={this.generateTest()}
+          itemContent={(index: any, item: any) => (
+
+            <div>
+              {item}
+            </div>
+              
+          )}
+        />
+
+        {/* <div className="allContentContainer">
+          <div className="contentContainer">  
+            <div className="content content-border">
+              <div className="listContainer">  */}
+                {/* PLACE VIRTUOSO IN HERE */}
+              {/* </div>
+            </div>
+          </div>
+        </div> */}
       </IonContent>
-    </IonPage>
-  );
-};
+    )
+
+  }
+
+
+}
 
 export default Tab1;
